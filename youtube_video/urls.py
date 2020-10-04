aa2390adad22bee1e7792d1f77b5b64d021112f2
om-12 <omkantmishra12@gmail.com>
2020-10-04 15:05:34 +0530

    youtube video player ready

--- youtube_video/urls.py
+++ youtube_video/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns=[
  
     path('',views.yvideo,name='yvideo'),
+    path('video/<int:id>', views.play_this_youtube,name='play_yvideo')
+    
     
 ]
--- youtube_video/views.py
+++ youtube_video/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Item
 # Create your views here.
 
@@ -6,6 +6,6 @@ def yvideo(request):
     obj=Item.objects.all()
     return render(request,'index.html',{'obj':obj})
 
-def play_this_youtube(request,url):
-    new_url=url
-    return render(request,'youtube_video_player.html',{'new_url':new_url})
+def play_this_youtube(request,id):
+    item = get_object_or_404(Item,pk = id)
+    return render(request,'youtube_video_player.html',{'new_url':item.video})
