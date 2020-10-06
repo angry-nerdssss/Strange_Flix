78dfc2cbc6bd8ed75df48ee0b30d22695f596126
om-12 <omkantmishra12@gmail.com>
2020-10-06 15:32:08 +0530

    youtube video upload complete part 1

--- youtube_video/models.py
+++ youtube_video/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from embed_video.fields import EmbedVideoField
-
+from taggit.managers import TaggableManager
+from datetime import date
 class Item(models.Model):
-    video = EmbedVideoField()  # same like models.URLField()
-    
+    video = models.URLField(max_length = 200)  
+    title=models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    publish_date= models.DateField(auto_now_add=True)
+    description=models.TextField(max_length=500)
+    slug = models.SlugField(unique=True, max_length=100)
+    tags = TaggableManager()
+    def __str__(self):
+        return self.video
--- youtube_video/urls.py
+++ youtube_video/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns=[
  
     path('',views.yvideo,name='yvideo'),
-    path('video/<int:id>', views.play_this_youtube,name='play_yvideo')
-    
-    
+    path('video/<int:id>/', views.play_this_youtube,name='play_yvideo'),
+    path('tag/<slug:slug>/',views.yvideo_tagged,name='tagged'),
+    path('detail/<slug:slug>/',views.yvideo_detail_view,name='detail'),
+    path('add_video/',views.yvideo_upload_view,name='add_youtube_video'),
 ]
