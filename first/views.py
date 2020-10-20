ee248b993c869b64a3c0d3c18dad7fae3a97f718
prashant <prashantpal0388@gmail.com>
2020-10-20 16:58:16 +0530

    slicing of videos done

--- first/views.py
+++ first/views.py
@@ -11,125 +11,137 @@ from storage_video.models import Video
 from youtube_video.models import Item
 from .models import Feedback
 
-#this function is to render to the main page when the user first searches for the site
-def index(request) :
+# this function is to render to the main page when the user first searches for the site
+
+
+def index(request):
     items = Item.objects.all()
     videos = Video.objects.all()
-    showRegister=False
-    showLogin=False
+    showRegister = False
+    showLogin = False
     context = {
-        'showRegister':showRegister,
-        'showLogin':showLogin,
-        'items':items,
-        'videos':videos,
+        'showRegister': showRegister,
+        'showLogin': showLogin,
+        'items': items,
+        'videos': videos,
     }
-    return render(request,"index.html",context)
+    return render(request, "index.html", context)
 
-#this function is to set login conditions and functionality
-def login(request) :
-     if request.method == 'POST' :
-         username = request.POST['username']
-         password = request.POST['password']
-          # by writing this we are checking whether the entered username and password are of the same user or not 
-         user = auth.authenticate(username=username,password=password)
-         if user is not None :
-             auth.login(request,user)
-             return redirect('/')
-         else :
-             messages.info(request,'invalid credentials')
-             context={
-                    'showRegister':False,
-                    'showLogin':True,
-               }
-             return render(request,'index.html',context)
-            
-     else :
-         context={
-                    'showRegister':False,
-                    'showLogin':True,
-               }
-         return render(request,'index.html',context)
+# this function is to set login conditions and functionality
 
 
-#this function is used to get user logged out
-def logout(request) :
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # by writing this we are checking whether the entered username and password are of the same user or not
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'invalid credentials')
+            context = {
+                'showRegister': False,
+                'showLogin': True,
+            }
+            return render(request, 'index.html', context)
+
+    else:
+        context = {
+            'showRegister': False,
+            'showLogin': True,
+        }
+        return render(request, 'index.html', context)
+
+
+# this function is used to get user logged out
+def logout(request):
     auth.logout(request)
     request.session['member_id'] = 0
     return redirect('/')
 
-##this function is to set register conditions and functionality
+# this function is to set register conditions and functionality
+
+
 def register(request):
-    if request.method == 'POST' :
-        username=request.POST['username']
+    if request.method == 'POST':
+        username = request.POST['username']
         email = request.POST['email']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
-        # by writing this condition we are checking that if password1 and password2 are equal or not 
-        if password1==password2 :
+        # by writing this condition we are checking that if password1 and password2 are equal or not
+        if password1 == password2:
             # by writing this condition we are checking that if this email is already registered or not
-            if User.objects.filter(email=email).exists() :
-               messages.info(request,'email taken already')
-               context={
-                    'showRegister':True,
-                    'showLogin':False,
-                    
-               }
-               return render(request,'index.html',context)
-            elif User.objects.filter(username=username).exists() :
-               messages.info(request,'username taken already')
-               context={
-                    'showRegister':True,
-                    'showLogin':False,
-               }
-               return render(request,'index.html',context)
-            else :
-                user =User.objects.create_user(username=username,email=email,password=password1)
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'email taken already')
+                context = {
+                    'showRegister': True,
+                    'showLogin': False,
+
+                }
+                return render(request, 'index.html', context)
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'username taken already')
+                context = {
+                    'showRegister': True,
+                    'showLogin': False,
+                }
+                return render(request, 'index.html', context)
+            else:
+                user = User.objects.create_user(
+                    username=username, email=email, password=password1)
                 # by writing this only we are hitting the database to store the information
                 user.save()
                 sendConfirm(user)
                 print('user created')
-                messages.info(request,'Please check your e-mail')
-                context={
-                    'showRegister':True,
-                    'showLogin':False,
-               }
-            return render(request,'index.html',context)
-        else :
-            messages.info(request,'password not matching')
-            context={
-                    'showRegister':True,
-                    'showLogin':False,
-                    
-               }
-            return render(request,'index.html',context)
-        return('/')  
-    else :
-        return render(request,'reg.html')
+                messages.info(request, 'Please check your e-mail')
+                context = {
+                    'showRegister': True,
+                    'showLogin': False,
+                }
+            return render(request, 'index.html', context)
+        else:
+            messages.info(request, 'password not matching')
+            context = {
+                'showRegister': True,
+                'showLogin': False,
+
+            }
+            return render(request, 'index.html', context)
+        return('/')
+    else:
+        return render(request, 'reg.html')
 
 
-#this function will simply reder you to subscription page
+# this function will simply reder you to subscription page
 def subscription(request):
-    return render(request,"subscription.html")
+    return render(request, "subscription.html")
+
 
 def show_feedback(request):
-    feedbacks=Feedback.objects.order_by('-publish_date')
-    context={
-        'feedbacks':feedbacks,
+    feedbacks = Feedback.objects.order_by('-publish_date')
+    context = {
+        'feedbacks': feedbacks,
     }
-    return render(request,'feedback.html',context)
+    return render(request, 'feedback.html', context)
 
-##this function is to take feedback
-def get_feedback(request) :
+# this function is to take feedback
+
+
+def get_feedback(request):
     name = request.POST['name']
     email = request.POST['email']
     subject = request.POST['subject']
     message = request.POST['message']
-    feed = Feedback(name=name,email=email,subject=subject,message=message)
+    feed = Feedback(name=name, email=email, subject=subject, message=message)
     # by writing this only we are hitting the database to store the information
     feed.save()
-    
+
     return redirect('show_feedback')
 
-#this function is to take user to about.html page
+# this function is to take user to about.html page
+
+
 def about(request):
-    return render(request,"about.html")
+    return render(request, "about.html")
--- storage_video/models.py
+++ storage_video/models.py
@@ -5,61 +5,65 @@ from taggit.managers import TaggableManager
 from datetime import date
 from django.contrib.contenttypes.fields import GenericRelation
 from comment.models import Comment
-#this model is to save all the details of the uploaded video from the admin
+# this model is to save all the details of the uploaded video from the admin
+
 
 class Video(models.Model):
-    #categories
-    MOVIES='movie'
-    TVSHOW='show'
-    TVSERIES='series'
+    # categories
+    MOVIES = 'movie'
+    TVSHOW = 'show'
+    TVSERIES = 'series'
 
-    #genre
-    FICTION='Fiction'
-    MYSTERY='Mystery'
-    THRILLER= 'Thriller'
-    HORROR='Horror'
-    HISTORICAL='Historical'
-    ROMANCE='Romance'
-    WESTERN='Western'
-    FANTASY='Fantasy'
-    ACTION='Action'
-    COMEDY='Comedy'
-    CRIME='Crime'
-    ADVENTURE='Adventure'
-    ANIMATION='Animation'
-    WAR='War'
+    # genre
+    FICTION = 'Fiction'
+    MYSTERY = 'Mystery'
+    THRILLER = 'Thriller'
+    HORROR = 'Horror'
+    HISTORICAL = 'Historical'
+    ROMANCE = 'Romance'
+    WESTERN = 'Western'
+    FANTASY = 'Fantasy'
+    ACTION = 'Action'
+    COMEDY = 'Comedy'
+    CRIME = 'Crime'
+    ADVENTURE = 'Adventure'
+    ANIMATION = 'Animation'
+    WAR = 'War'
 
     CATEGORY_CHOICES = [
-        
+
         (MOVIES, 'Movies'),
         (TVSHOW, 'TV Show'),
         (TVSERIES, 'TV Series'),
     ]
     GENRE_CHOICES = [
-        
+
         (FICTION, 'Fiction'),
         (MYSTERY, 'Mystery'),
         (THRILLER, 'Thriller'),
-        (HORROR,'Horror'),
-        (HISTORICAL,'Historical'),
-        (ROMANCE,'Romance'),
-        (WESTERN,'Western'),
-        (FANTASY,'Fantasy'),
-        (ACTION,'Action'),
-        (COMEDY,'Comedy'),
-        (CRIME,'Crime'),
-        (ADVENTURE,'Adventure'),
-        (ANIMATION,'Animation'),
-        (WAR,'War'),
+        (HORROR, 'Horror'),
+        (HISTORICAL, 'Historical'),
+        (ROMANCE, 'Romance'),
+        (WESTERN, 'Western'),
+        (FANTASY, 'Fantasy'),
+        (ACTION, 'Action'),
+        (COMEDY, 'Comedy'),
+        (CRIME, 'Crime'),
+        (ADVENTURE, 'Adventure'),
+        (ANIMATION, 'Animation'),
+        (WAR, 'War'),
     ]
-    videofile= models.FileField(upload_to='videos/', null=True, verbose_name="")
-    poster= models.FileField(upload_to='images/', null=True, verbose_name="")
-    title=models.CharField(max_length=100)
-    description=models.TextField(max_length=500)
-    category = models.CharField(max_length=20,choices=CATEGORY_CHOICES,default=MOVIES,)
-    genre=models.CharField(max_length=20,choices=GENRE_CHOICES,default=FICTION,)
+    videofile = models.FileField(
+        upload_to='videos/', null=True, verbose_name="")
+    poster = models.FileField(upload_to='images/', null=True, verbose_name="")
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    category = models.CharField(
+        max_length=20, choices=CATEGORY_CHOICES, default=MOVIES,)
+    genre = models.CharField(
+        max_length=20, choices=GENRE_CHOICES, default=FICTION,)
     publish_date = models.DateField(auto_now_add=True)
-    slug= models.SlugField(unique=True,max_length=100)
+    slug = models.SlugField(unique=True, max_length=100)
     tags = TaggableManager()
     comments = GenericRelation(Comment)
 
@@ -70,20 +74,28 @@ class Video(models.Model):
         return self.dis_likes.users.count()
 
     def __str__(self):
-            return self.video
+        return self.title
+
 
 class Like(models.Model):
-    users=models.ManyToManyField(User,related_name='requirement_video_likes')
-    video = models.OneToOneField(Video, related_name="likes", on_delete=models.CASCADE)
+    users = models.ManyToManyField(
+        User, related_name='requirement_video_likes')
+    video = models.OneToOneField(
+        Video, related_name="likes", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return str(self.video.title)[:30]
 
+
 class Dislike(models.Model):
-    users=models.ManyToManyField(User,related_name='requirement_video_dis_likes')
-    video = models.OneToOneField(Video, related_name="dis_likes", on_delete=models.CASCADE)
+    users = models.ManyToManyField(
+        User, related_name='requirement_video_dis_likes')
+    video = models.OneToOneField(
+        Video, related_name="dis_likes", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return str(self.video.title)[:30]
