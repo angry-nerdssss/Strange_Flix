c4b950fc7da0e79d272dcebfd62e809cb0e86e3e
Puneet <puneet5may@gmail.com>
2020-10-21 20:49:32 +0530

    video quality change added

--- static/js/video.js
+++ static/js/video.js
@@ -1,9 +1,8 @@
-
 var player=videojs('videoPlayer',{
 	autoplay:'unmuted',
 	controls:true,
 	loop:true,
-	playbackRates:[0.25,0.5,0.75,1.0,1.25,1.5,1.75,2.0],
+  playbackRates:[0.25,0.5,0.75,1.0,1.25,1.5,1.75,2.0],
 });
 player.ready(function() {
   this.hotkeys({
@@ -12,3 +11,5 @@ player.ready(function() {
     enableModifiersForNumbers: false
   });
 });
+
+player.controlBar.addChild('QualitySelector');
