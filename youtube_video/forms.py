b064aef171dcc45cbc91d9880ca80561615fd26d
prashant <prashantpal0388@gmail.com>
2020-10-25 19:03:06 +0530

    favicon and del added

--- youtube_video/forms.py
+++ youtube_video/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from .models import Item
 
-#this form is to take input of the youtube video linkes and the video details
+# this form is to take input of the youtube video linkes and the video details
+
+
 class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
