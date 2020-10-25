03bc9f732c3ac5e492500a761092c9e52f407992
Puneet <puneet5may@gmail.com>
2020-10-25 20:43:14 +0530

    flagging of video done

--- storage_video/views.py
+++ storage_video/views.py
@@ -3,7 +3,7 @@ from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User, auth
 from django.contrib.auth.decorators import login_required
 from taggit.models import Tag
-from .models import Video,Time
+from .models import Video, Time
 from first.models import Subscription
 from .forms import VideoForm
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -16,7 +16,9 @@ try:
 except ImportError:
     import json
 from django.views.decorators.http import require_POST
-#this function is to upload the svideo_upload.html file when the user fills the form to upload video
+# this function is to upload the svideo_upload.html file when the user fills the form to upload video
+
+
 def svideo_upload_view(request):
     # we are showing all the uploaded videos according to their publish dates
     videos = Video.objects.order_by('-publish_date')
@@ -61,39 +63,41 @@ def svideo_tagged(request, slug):
 # this function is to play the selected video
 
 
-#this function is to play the selected video
-def video(request,id):
+# this function is to play the selected video
+def video(request, id):
     try:
-        current_user=User.objects.get(username=request.user.username)
+        current_user = User.objects.get(username=request.user.username)
     except:
         return redirect('index')
-    current_user=User.objects.get(username=request.user.username)
+    current_user = User.objects.get(username=request.user.username)
     if(current_user.is_superuser):
-        current_time=0.0
-        video =get_object_or_404(Video,pk=id)
-        if Time.objects.filter(username=current_user.username).filter(video_title=video.title).exists() :
-            obj=Time.objects.filter(username=current_user.username).filter(video_title=video.title)[:1].get()
-            current_time=obj.currentTime
-        
-        context={
-            'video':video,
-            'current_time':current_time,
+        current_time = 0.0
+        video = get_object_or_404(Video, pk=id)
+        if Time.objects.filter(username=current_user.username).filter(video_title=video.title).exists():
+            obj = Time.objects.filter(username=current_user.username).filter(
+                video_title=video.title)[:1].get()
+            current_time = obj.currentTime
+
+        context = {
+            'video': video,
+            'current_time': current_time,
         }
         return render(request, 'video.html', context)
 
-    subscription=Subscription.objects.get(user=request.user)
+    subscription = Subscription.objects.get(user=request.user)
     if subscription.paid == 'False':
         return redirect('subscription')
-    
-    video =get_object_or_404(Video,pk=id)
-    current_time=0.0
-    if Time.objects.filter(username=current_user.username).filter(video_title=video.title).exists() :
-        obj=Time.objects.filter(username=current_user.username).filter(video_title=video.title)[:1].get()
-        current_time=obj.currentTime
-        
-    context={
-        'video':video,
-        'current_time':current_time,
+
+    video = get_object_or_404(Video, pk=id)
+    current_time = 0.0
+    if Time.objects.filter(username=current_user.username).filter(video_title=video.title).exists():
+        obj = Time.objects.filter(username=current_user.username).filter(
+            video_title=video.title)[:1].get()
+        current_time = obj.currentTime
+
+    context = {
+        'video': video,
+        'current_time': current_time,
     }
     return render(request, 'video.html', context)
 
@@ -147,14 +151,14 @@ def svideo_like(request):
         user = request.user
         slug = request.POST.get('slug', None)
         video = get_object_or_404(Video, slug=slug)
-        liked=True
-        disliked=False
+        liked = True
+        disliked = False
         if video.likes.filter(id=user.id).exists():
             # user has already liked this video
             # remove like/user
             video.likes.remove(user)
             message = 'You disliked this'
-            liked=False
+            liked = False
         else:
             # add a new like for a video
             video.likes.add(user)
@@ -163,7 +167,8 @@ def svideo_like(request):
 
             message = 'You liked this'
 
-    ctx = {'likes_count': video.total_likes, 'message': message,'dislikes_count': video.total_dislikes,'liked':liked,'disliked':disliked}
+    ctx = {'likes_count': video.total_likes, 'message': message,
+           'dislikes_count': video.total_dislikes, 'liked': liked, 'disliked': disliked}
     # use mimetype instead of content_type if django < 5
     return HttpResponse(json.dumps(ctx), content_type='application/json')
     return HttpResponseRedirect(reverse('play_svideo', args=[str(video.id)]))
@@ -175,13 +180,13 @@ def svideo_dislike(request):
         user = request.user
         slug = request.POST.get('slug', None)
         video = get_object_or_404(Video, slug=slug)
-        liked=False
-        disliked=True
+        liked = False
+        disliked = True
         if video.dislikes.filter(id=user.id).exists():
             # user has already liked this video
             # remove like/user
             video.dislikes.remove(user)
-            disliked=False
+            disliked = False
             message = 'You disliked this'
         else:
             # add a new like for a video
@@ -190,14 +195,15 @@ def svideo_dislike(request):
                 video.likes.remove(user)
             message = 'You liked this'
 
-    ctx = {'dislikes_count': video.total_dislikes, 'message': message,'likes_count': video.total_likes,'liked':liked,'disliked':disliked}
+    ctx = {'dislikes_count': video.total_dislikes, 'message': message,
+           'likes_count': video.total_likes, 'liked': liked, 'disliked': disliked}
     # use mimetype instead of content_type if django < 5
     return HttpResponse(json.dumps(ctx), content_type='application/json')
     return HttpResponseRedirect(reverse('play_svideo', args=[str(video.id)]))
 
 
 def favourite_svideo(request):
-    title = request.GET.get('title',None)
+    title = request.GET.get('title', None)
     try:
         currentUser = User.objects.get(username=request.user.username)
 
@@ -205,48 +211,51 @@ def favourite_svideo(request):
         print("y")
         return redirect('login')
     currentUser = User.objects.get(username=request.user.username)
-    added=True
-    video=Video.objects.get(title=title)
+    added = True
+    video = Video.objects.get(title=title)
     print(video.title)
     print(" ")
     print(video.description)
     if video.favourite.filter(id=currentUser.id).exists():
         video.favourite.remove(request.user)
-        added=False
+        added = False
     else:
         video.favourite.add(request.user)
-    ctx={'added':added,}
+    ctx = {'added': added, }
     return HttpResponse(json.dumps(ctx), content_type='application/json')
 
 
 def getCurrentTime(request):
-    current_time=request.GET.get('currentTime',None)
-    title=request.GET.get('title',None)
+    current_time = request.GET.get('currentTime', None)
+    title = request.GET.get('title', None)
     try:
         current_user = User.objects.get(username=request.user.username)
     except:
         return redirect('login')
     current_user = User.objects.get(username=request.user.username)
-    if Time.objects.filter(username=current_user.username).filter(video_title=title).exists() :
-        time=Time.objects.filter(username=current_user.username).filter(video_title=title)[:1].get()
-        time.currentTime=current_time
+    if Time.objects.filter(username=current_user.username).filter(video_title=title).exists():
+        time = Time.objects.filter(username=current_user.username).filter(
+            video_title=title)[:1].get()
+        time.currentTime = current_time
         time.save()
-    else :
-        time=Time.objects.create(username=current_user.username,video_title=title,currentTime=current_time)
-    ctx={}
+    else:
+        time = Time.objects.create(
+            username=current_user.username, video_title=title, currentTime=current_time)
+    ctx = {}
     return HttpResponse(json.dumps(ctx), content_type='application/json')
-    
+
+
 def increase_views(request):
-    title=request.GET.get('title',None)
+    title = request.GET.get('title', None)
     try:
         current_user = User.objects.get(username=request.user.username)
     except:
         return redirect('login')
-    video=Video.objects.get(title=title)
-    video.views=video.views+1
+    video = Video.objects.get(title=title)
+    video.views = video.views+1
     print(video.views)
     video.save()
-    ctx={}
+    ctx = {}
     return HttpResponse(json.dumps(ctx), content_type='application/json')
 
 
@@ -258,7 +267,7 @@ def delete_svideo(request, id):
 
     # fetch the object related to passed id
     context = {}
-    obj = get_object_or_404(Video,id=id)
+    obj = get_object_or_404(Video, id=id)
 
     # delete object
 
@@ -269,3 +278,26 @@ def delete_svideo(request, id):
     # home page
 
     return HttpResponseRedirect("/")
+
+
+def flag_svideo(request):
+    title = request.GET.get('title', None)
+    try:
+        currentUser = User.objects.get(username=request.user.username)
+
+    except:
+        print("y")
+        return redirect('login')
+    currentUser = User.objects.get(username=request.user.username)
+    added = True
+    video = Video.objects.get(title=title)
+    print(video.title)
+    print(" ")
+    print(video.description)
+    if video.flag.filter(id=currentUser.id).exists():
+        video.flag.remove(request.user)
+        added = False
+    else:
+        video.flag.add(request.user)
+    ctx = {'added': added, }
+    return HttpResponse(json.dumps(ctx), content_type='application/json')
