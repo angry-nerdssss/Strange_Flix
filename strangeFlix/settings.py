0ce5001704bb1ad43688167d9c9c95a486028897
om-12 <omkantmishra12@gmail.com>
2020-11-01 16:32:42 +0530

    login in for always removed

--- strangeFlix/settings.py
+++ strangeFlix/settings.py
@@ -120,7 +120,7 @@ WSGI_APPLICATION = 'strangeFlix.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'StrangeFlix2',
+        'NAME': 'StrangeFlix',
         'USER': 'postgres',
         'PASSWORD': '1234',
         'HOST': 'localhost',
